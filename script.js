80ea0a9f0aa1469b0d442c5f4dda9afe1afb799f
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-12-13 09:40:15 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
