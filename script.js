25c54e82d27893f58c31458833977387c89c18f1
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-12-15 14:33:08 -0600

    Set up css and words-js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+import { WORDS } from "./words.js";
+
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+
+console.log(rightGuessString);
+
+function initBoard() {
+  let board = document.getElementById("game-board");
+
+  
+}
