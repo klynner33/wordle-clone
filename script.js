7c383b8d3d7816123ee8186dedce8733f2f82493
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-12-21 08:53:32 -0600

    Added key press feature to guess letter on the game board

--- script.js
+++ script.js
@@ -26,3 +26,30 @@ function initBoard() {
 }
 
 initBoard()
+
+// Accepting user input
+
+document.addEventListener("keyup", (e) => {
+
+  if (guessesRemaining === 0) {
+      return
+  }
+
+  let pressedKey = String(e.key)
+  if (pressedKey === "Backspace" && nextLetter !== 0) {
+      deleteLetter()
+      return
+  }
+
+  if (pressedKey === "Enter") {
+      checkGuess()
+      return
+  }
+
+  let found = pressedKey.match(/[a-z]/gi)
+  if (!found || found.length > 1) {
+      return
+  } else {
+      insertLetter(pressedKey)
+  }
+})
