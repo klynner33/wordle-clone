5ae1c3a1f93236c8c83b30ef91fc3f2a5cf4f135
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-12-27 13:53:35 -0600

    Finished the checkGuess function

--- script.js
+++ script.js
@@ -95,3 +95,51 @@ function checkGuess () {
       alert("Word not in list!")
       return
   }
+
+  for (let i = 0; i < 5; i++) {
+    let letterColor = ''
+    let box = row.children[i]
+    let letter = currentGuess[i]
+    
+    let letterPosition = rightGuess.indexOf(currentGuess[i])
+    // is letter in the correct guess
+    if (letterPosition === -1) {
+        letterColor = 'grey'
+    } else {
+        // now, letter is definitely in word
+        // if letter index and right guess index are the same
+        // letter is in the right position 
+        if (currentGuess[i] === rightGuess[i]) {
+            // shade green 
+            letterColor = 'green'
+        } else {
+            // shade box yellow
+            letterColor = 'yellow'
+        }
+
+        rightGuess[letterPosition] = "#"
+    }
+
+    let delay = 250 * i
+    setTimeout(()=> {
+        //shade box
+        box.style.backgroundColor = letterColor
+        shadeKeyBoard(letter, letterColor)
+    }, delay)
+}
+
+if (guessString === rightGuessString) {
+    alert("You guessed right! Game over!")
+    guessesRemaining = 0
+    return
+} else {
+    guessesRemaining -= 1;
+    currentGuess = [];
+    nextLetter = 0;
+
+    if (guessesRemaining === 0) {
+        alert("You've run out of guesses! Game over!")
+        alert(`The right word was: "${rightGuessString}"`)
+    }
+}
+}
